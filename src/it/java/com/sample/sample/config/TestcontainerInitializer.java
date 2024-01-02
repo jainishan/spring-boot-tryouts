44c47c23c818c4414d3cdf20b3a6857284cfcf26
ishanjain <ishanjain@jajafinance.com>
2024-01-02 17:07:44 +0100

    work in progress for adding testcontainers for integration tests

--- src/it/java/com/sample/sample/config/TestcontainerInitializer.java
+++ src/it/java/com/sample/sample/config/TestcontainerInitializer.java
@@ -13,6 +13,7 @@ import org.testcontainers.containers.wait.strategy.HttpWaitStrategy;
 import org.testcontainers.images.PullPolicy;
 import org.testcontainers.lifecycle.Startables;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.vault.VaultContainer;
 
 import java.time.Duration;
 import java.util.Map;
@@ -27,11 +28,15 @@ public final class TestcontainerInitializer implements ApplicationContextInitial
     public static final String VAULT_DOCKER_IMAGE = "vault";
     public static final String VAULT_DOCKER_IMAGE_VERSION = "1.10.7";
     public static final String MYSQL_DOCKER_IMAGE = "mysql";
-    public static final String MYSQL_DOCKER_IMAGE_VERSION = "5.7.32";
+    public static final String MYSQL_DOCKER_IMAGE_VERSION = "5.7";
+    public static final String MYSQL_DATABASE_NAME = "trout";
+    public static final String MYSQL_DATASOURCE_ADDITIONAL_URL_PARAMS = "?createDatabaseIfNotExist=true";
+    public static final String MYSQL_ROOT_USER = "root";
+    public static final String MYSQL_ROOT_USER_PASSWORD = "test";
 
     public static final MySQLContainer mySQLContainer;
     private static final KafkaContainer kafkaContainer;
-//    private static final VaultContainer<?> vaultContainer;
+    private static final VaultContainer<?> vaultContainer;
     private static final ConsulContainer consulContainer;
 
     static {
@@ -46,20 +51,19 @@ public final class TestcontainerInitializer implements ApplicationContextInitial
                         .waitingFor(new HttpWaitStrategy().forPort(8500)
                                 .withStartupTimeout(Duration.ofSeconds(5))).withReuse(true);
 
-//        vaultContainer =
-//                new VaultContainer<>(DockerImageName.parse(VAULT_DOCKER_IMAGE).withTag(VAULT_DOCKER_IMAGE_VERSION))
-//                        .withVaultToken("test-root-token")
-//                        .withReuse(true)
-//                        .withClasspathResourceMapping("testdata/feature_flags.json",
-//                                "feature_flags.json",
-//                                BindMode.READ_ONLY)
-//                        .withSecretInVault("secret/apps/" + "sampleapp", "feature=true")
-//                        .withInitCommand("login test-root-token")
-//                        .withInitCommand("secrets enable -version=2 kv")
-//                        .withInitCommand("secrets enable -path=secrets kv")
-//                        .withInitCommand("kv put -format=json secrets/apps/global/features @feature_flags.json");
-//        Startables.deepStart(vaultContainer, consulContainer, kafkaContainer, mySQLContainer).join();
-        Startables.deepStart(consulContainer, kafkaContainer, mySQLContainer).join();
+        vaultContainer =
+                new VaultContainer<>(DockerImageName.parse(VAULT_DOCKER_IMAGE).withTag(VAULT_DOCKER_IMAGE_VERSION))
+                        .withVaultToken("test-root-token")
+                        .withReuse(true)
+                        .withClasspathResourceMapping("testdata/feature_flags.json",
+                                "feature_flags.json",
+                                BindMode.READ_ONLY)
+                        .withSecretInVault("secret/apps/" + "sampleapp", "feature=true")
+                        .withInitCommand("login test-root-token")
+                        .withInitCommand("secrets enable -version=2 kv")
+                        .withInitCommand("secrets enable -path=secrets kv")
+                        .withInitCommand("kv put -format=json secrets/apps/global/features @feature_flags.json");
+        Startables.deepStart(vaultContainer, consulContainer, kafkaContainer, mySQLContainer).join();
     }
 
     public static String getKafkaContainerBootstrapServer() {
@@ -70,16 +74,22 @@ public final class TestcontainerInitializer implements ApplicationContextInitial
     public void initialize(final ConfigurableApplicationContext applicationContext) {
         applicationContext.getEnvironment().getPropertySources().addFirst(
                 new MapPropertySource("mysqlContainer", Map.of(
-                        "jaja.kafka.bootstrap.servers", kafkaContainer.getBootstrapServers(),
-//                        "spring.cloud.vault.port", vaultContainer.getFirstMappedPort(),
                         "spring.cloud.consul.port", consulContainer.getFirstMappedPort(),
-                        "spring.datasource.url",
-                        "jdbc:mysql://" + mySQLContainer.getHost() + ":" + mySQLContainer.getFirstMappedPort() +
-                        "/tryout?createDatabaseIfNotExist=true&user=root&password=test",
-//                        "spring.datasource.username", mySQLContainer.getUsername(),
-//                        "spring.datasource.password", mySQLContainer.getPassword(),
+                        "jaja.kafka.bootstrap.servers", kafkaContainer.getBootstrapServers(),
+                        "spring.cloud.vault.uri", vaultContainer.getHttpHostAddress(),
+                        "spring.cloud.vault.port", vaultContainer.getFirstMappedPort(),
+                        "spring.cloud.vault.token", vaultContainer.getEnvMap().get("VAULT_TOKEN"),
+                        "spring.cloud.vault.authentication", "TOKEN",
+                        "spring.datasource.url", prepareDatasourceUrl(),
+                        "spring.datasource.username", MYSQL_ROOT_USER,
+                        "spring.datasource.password", MYSQL_ROOT_USER_PASSWORD,
                         "spring.datasource.driver-class-name", mySQLContainer.getDriverClassName()
                 ))
         );
+    }
+
+    private String prepareDatasourceUrl() {
+        return "jdbc:mysql://" + mySQLContainer.getHost() + ":" + mySQLContainer.getFirstMappedPort() + "/"
+               + MYSQL_DATABASE_NAME + MYSQL_DATASOURCE_ADDITIONAL_URL_PARAMS;
     }
 }
